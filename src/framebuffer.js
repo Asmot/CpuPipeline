61fc7f7046eaf481f68307dff26838a3f110ccb2
Xiangyu Zheng <xiangyu.zheng@shopee.com>
2022-05-17 22:17:59 +0800

    feat:init proj add frame buffer

---
+++ src/framebuffer.js
@@ -0,0 +1,58 @@
+
+class Color {
+    constructor(r,g,b,a) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a;
+    }
+}
+
+class FrameBuffer {
+    constructor(width, height, clearColor = new Color(1, 1, 1, 1)) {
+        this.width = width;
+        this.height = height;
+        this.bufferArray = [];
+        this.bufferArray.length = width * height;
+        for (let x = 0; x < width; x++) {
+            for (let y = 0; y < height; y++) {
+                this.bufferArray[y * width + x] = clearColor;
+            }
+            
+        }
+    }
+}
+
+const rgbaToHex = (r, g, b, a) => '#' + [r, g, b, a].map(x => {
+    const hex = x.toString(16)
+    return hex.length === 1 ? '0' + hex : hex
+}).join('')
+
+function colorRGB2Hex(color) {
+    let r = color.r * 255;
+    let g = color.g * 255;
+    let b = color.b * 255;
+    let a = color.a * 255;
+
+    let hex = rgbaToHex(r, g, b, a);
+    return hex;
+}
+
+// array to image
+var canvas = document.querySelector('#mycanvas');
+var ctx = canvas.getContext("2d");
+
+// array is color buffer
+function array_to_frame(frameBuffer) {
+    var width = frameBuffer.width;
+    var height = frameBuffer.width;
+
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            let color = frameBuffer.bufferArray[y * width + x];
+            ctx.fillStyle = colorRGB2Hex(color);
+            ctx.fillRect(x, y, 1, 1);
+        }
+        
+    }
+}
---
+++ src/main.js
@@ -0,0 +1,12 @@
+
+var width = 400;
+var height = 400;
+
+main();
+
+function main() {
+
+    var frameBuffer = new FrameBuffer(width, height, new Color(1, 1, 1, 1));
+    array_to_frame(frameBuffer)
+
+}
